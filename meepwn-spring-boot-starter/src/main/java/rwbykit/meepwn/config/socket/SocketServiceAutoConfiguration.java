7b6e0bf403e506b67cdbef08a07b9292ebd0fefd
rwbykit <rwbykit@163.com>
2020-03-31 17:46:18 +0800

    remove some codes

--- meepwn-spring-boot-starter/src/main/java/rwbykit/meepwn/config/socket/SocketServiceAutoConfiguration.java
+++ meepwn-spring-boot-starter/src/main/java/rwbykit/meepwn/config/socket/SocketServiceAutoConfiguration.java
@@ -25,5 +25,4 @@ public class SocketServiceAutoConfiguration {
         AsynchronousScheduler.setThreadPool(threadPool);
     }
 
-
 }
--- meepwn-test/src/main/java/rwbykit/meepwn/test/Application.java
+++ meepwn-test/src/main/java/rwbykit/meepwn/test/Application.java
@@ -3,6 +3,7 @@ package rwbykit.meepwn.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import rwbykit.meepwn.test.service.Client;
 
 
 @SpringBootApplication(scanBasePackages = "rwbykit.meepwn")
@@ -10,6 +11,8 @@ public class Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        Client client = context.getBean(Client.class);
+        client.send();
     }
 
 
